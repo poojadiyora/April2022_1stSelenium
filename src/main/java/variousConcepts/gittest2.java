9585a4535e179b891a92732ef185f588255073f6
poojadiyora <POOJADIYORA07@GMAIL.COM>
2022-08-28 00:32:09 -0400

    commit ment

--- src/main/java/variousConcepts/gittest2.java
+++ src/main/java/variousConcepts/gittest2.java
@@ -3,8 +3,18 @@ package variousConcepts;
 public class gittest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	printme();
+	printme2();
 	}
-
+	public static void printme() {
+		System.out.println("GIT TESTING");
+	}
+	public static void printme2() {
+		System.out.println("GIT TESTING");
+		System.out.println("GIT TESTING2");
+		System.out.println("GIT TESTING3");
+		System.out.println("GIT TESTING4");
+		System.out.println("GIT TESTING5");
+		System.out.println("GIT TESTING6");
+	}
 }
